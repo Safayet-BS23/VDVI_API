4011011915cbea8f96cb2a239c20f2276f6be816
nahidbs23 <nahid.hasan@bs-23.net>
2022-11-21 14:46:38 +0600

    Banqueting Room Types fix issue

--- VDVI/VDVI.Services/Services/ApmaServices/HcsListBanquetingRoomTypesService.cs
+++ VDVI/VDVI.Services/Services/ApmaServices/HcsListBanquetingRoomTypesService.cs
@@ -3,12 +3,10 @@ using Framework.Core.Base.ModelEntity;
 using Framework.Core.Exceptions;
 using Framework.Core.Utility;
 using SOAPService;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VDVI.DB.Dtos;
-using VDVI.Repository.DB;
 using VDVI.Services.Interfaces;
 
 namespace VDVI.Services
@@ -24,38 +22,39 @@ namespace VDVI.Services
 
         public async Task<Result<PrometheusResponse>> HcsListBanquetingRoomTypesAsync()
         {
-            var result = new Result<PrometheusResponse>();
+            return await TryCatchExtension.ExecuteAndHandleErrorAsync(
+              async () =>
+              {
+                  Authentication pmsAuthentication = GetApmaAuthCredential();
+                  HcsListBanquetingRoomTypesResponse res = new HcsListBanquetingRoomTypesResponse();
+                  List<BanquetingRoomTypesDto> dto = new List<BanquetingRoomTypesDto>();
 
-            //return await TryCatchExtension.ExecuteAndHandleErrorAsync(
-            //  async () =>
-            //  {
-            //      Authentication pmsAuthentication = GetApmaAuthCredential();
-            //      HcsListBanquetingRoomTypesResponse res = new HcsListBanquetingRoomTypesResponse();
-            //      List<BanquetingRoomTypesDto>  dto = new List<BanquetingRoomTypesDto>(); 
-
-            //      for (int i = 0; i < ApmaProperties.Length; i++)
-            //      {
-            //          var propertyCode = ApmaProperties[i];
-            //          res = await client.HcsListBanquetingRoomTypesAsync(pmsAuthentication, propertyCode, "", "");
-            //          List<ListBanquetingRoomTypesItem> types = res.HcsListBanquetingRoomTypesResult.BanquetingRoomTypes.ToList();
-            //          FormatSummaryObject( types, dto, propertyCode);
-            //      }
-            //      if (dto.Count > 0)
-            //          //DB operation
-            //          //var d = await _hcsListBanquetingRoomTypeService.BulkInsertWithProcAsync(dto);
-
-            //      return PrometheusResponse.Success("", "Data retrieval is successful");
-            //  },
-            //  exception => new TryCatchExtensionResult<Result<PrometheusResponse>>
-            //  {
-            //      DefaultResult = PrometheusResponse.Failure($"Error message: {exception.Message}. Details: {ExceptionExtension.GetExceptionDetailMessage(exception)}"),
-            //      RethrowException = false
-            //  });
-            return result;
+                  for (int i = 0; i < ApmaProperties.Length; i++)
+                  {
+                      var propertyCode = ApmaProperties[i];
+                      res = await client.HcsListBanquetingRoomTypesAsync(pmsAuthentication, propertyCode, "", "");
+                      List<ListBanquetingRoomTypesItem> types = res.HcsListBanquetingRoomTypesResult.BanquetingRoomTypes.ToList();
+                      dto = FormatSummaryObject(types, propertyCode);
+                  }
+                  if (dto.Count > 0)
+                  {
+                      //DB operation
+                      var result = await _hcsListBanquetingRoomTypeService.BulkInsertWithProcAsync(dto);
+                  }        
+                      
+                  return PrometheusResponse.Success("", "Data retrieval is successful");
+              },
+              exception => new TryCatchExtensionResult<Result<PrometheusResponse>>
+              {
+                  DefaultResult = PrometheusResponse.Failure($"Error message: {exception.Message}. Details: {ExceptionExtension.GetExceptionDetailMessage(exception)}"),
+                  RethrowException = false
+              });
         }
 
-        private void FormatSummaryObject(List<ListBanquetingRoomTypesItem> types, List<BanquetingRoomTypesDto> BanquetingRoomTypesdto, string propertyCode)
+        private List<BanquetingRoomTypesDto> FormatSummaryObject(List<ListBanquetingRoomTypesItem> types, string propertyCode)
         {
+            List<BanquetingRoomTypesDto> BanquetingRoomTypesdto = new List<BanquetingRoomTypesDto>();
+
             foreach (var item in BanquetingRoomTypesdto)
             {
                 var dto = new BanquetingRoomTypesDto()
@@ -65,9 +64,9 @@ namespace VDVI.Services
                     Code=item.Code
                 };
                 BanquetingRoomTypesdto.Add(dto);
-
-
             }
+
+            return BanquetingRoomTypesdto;
         }
          
     }
